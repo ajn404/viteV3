817be18f0f96316798fb11f713dbd9bcfb63efe2
n-graymoon <huiyue.ning@71360.com>
2021-11-25 15:27:56 +0800

    no message

--- assets/vuexl.452c9d80.js
+++
@@ -1,1 +0,0 @@
-import{u as s,o,c as t,a as r}from"./vendor.d4de65db.js";import{_ as a}from"./index.6123c363.js";const _={setup(){const e=s();console.log("store",e),console.log("getter",e.getters)}},c={class:"about"},n=r("h1",null,"This is a vuex page",-1),d=[n];function l(e,u,i,p,v,x){return o(),t("div",c,d)}var h=a(_,[["render",l]]);export{h as default};
