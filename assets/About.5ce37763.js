1d121f3f6ab7055c9b80b1713d9efb4ed2f51daf
ajn404 <huiyuening00@gmail.com>
2021-11-28 23:40:07 +0800

    Add files via upload

---
+++ assets/About.5ce37763.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.477e88cd.js";import{o,c as t,a as s}from"./vendor.d4de65db.js";const _={},a={class:"about"},c=s("h1",null,"This is an about page",-1),n=[c];function r(d,i){return o(),t("div",a,n)}var f=e(_,[["render",r]]);export{f as default};
