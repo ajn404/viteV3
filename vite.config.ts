1bb28b063b672c580be70172de32a06434fc6f4e
mac_ajn404 <huiyuening00@gmail.com>
2021-11-28 23:25:31 +0800

    no message

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx()],
+  server:{
+    port:3030,
+  },
   resolve: {
     alias: {
       '@/': new URL('./src/', import.meta.url).pathname
